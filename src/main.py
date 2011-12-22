4fe03e248221e068eb774893e2a44661a3896e21
jrupac <ajay.roopakalu@gmail.com>
2011-12-22 14:04:15 -0500

    Switched to async email forwarding model for improved performance and reduced CPU consumption; deleted cron jobs and task queues.

--- src/main.py
+++ src/main.py
@@ -4,10 +4,10 @@ from GoogleVoice import GoogleVoice
 from Config import Config
 
 from HTMLParser import HTMLParser
-from google.appengine.api import taskqueue
 from google.appengine.api import xmpp
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp.mail_handlers import InboundMailHandler
 
 HOSTNAME = '@gv-commander.appspotchat.com'
 
@@ -41,77 +41,31 @@ class XMPPHandler(webapp.RequestHandler):
             if voice.send_sms(result.group(0), message.body).status != 200:
                 message.reply("ERROR: Could not send message.")
 
-class GVHandler(webapp.RequestHandler):
-    def get(self):
+class MailHandler(InboundMailHandler):
+    def receive(self, mail_message):
         global KILL_SWITCH
-        if KILL_SWITCH:
-            return
+        global XMPP_USERNAME
         
-        taskqueue.add(url='/worker', params={'recurse' : 5})
-
-class TaskHandler(webapp.RequestHandler):
-    def post(self):
-        global OLD_MESSAGES
-        global KILL_SWITCH
-
+        # Do nothing if disabled
         if KILL_SWITCH:
             return
 
-        recurse_step = int(self.request.get('recurse'))
+        # Try to find the address we're going to
+        result = TO.search(mail_message.sender)
 
-        if recurse_step == 0:
-            return
-
-        # Grab the current conversations and phone numbers in inbox
-        numbers, all_messages = voice.check_sms()
-
-        # First-run. Just collect everything and return
-        if OLD_MESSAGES == None:
-            OLD_MESSAGES = dict(all_messages)
-            return
-
-        # Iterate over all conversations currently in the inbox
-        for key in all_messages.keys():
-
-            from_addr = numbers[key] + HOSTNAME
-
-            # This is a conversation which existed at the last poll
-            if key in OLD_MESSAGES.keys():
-
-                # The last message in the old list for this conversation
-                last_old_message = OLD_MESSAGES[key][-1]
-                last_index = -1
-               
-                # Find the location of the last old message in the new list
-                for index, val in enumerate(all_messages[key]):
-                    if val == last_old_message:
-                        last_index = index
-                        break
-                
-                # Send off all the new messages in this conversation
-                for message in all_messages[key][last_index+1:]:
-                    OLD_MESSAGES[key].append(message)
-                    if message['author'] != 'Me:':
-                        xmpp.send_message(XMPP_USERNAME, 
-                                          HP.unescape(message['text']), 
-                                          from_jid=from_addr)
-            # This is a completely new conversation first seen in this poll
-            else:
-                new_conversation = []
-                for message in all_messages[key]:
-                    new_conversation.append(message)
-                    if message['author'] != 'Me:':
-                        xmpp.send_message(XMPP_USERNAME, 
-                                          HP.unescape(message['text']), 
-                                          from_jid=from_addr)
-                OLD_MESSAGES[key] = new_conversation
-        
-        # Recursive enqueue
-        taskqueue.add(url='/worker', params={'recurse' : recurse_step - 1})
+        # If successful, parse full body and send it with the appropriate 
+        # originating JID
+        if result:
+            body = ''.join(body[1].decode() for body in mail_message.bodies('text/plain'))
+            xmpp.send_message(XMPP_USERNAME, body, 
+                              from_jid='+' + result.group(0) + HOSTNAME)
+        # Otherwise, report an error
+        else:
+            xmpp.send_message(XMPP_USERNAME, "ERROR: Invalid sender: " + 
+                              mail_message.sender)
 
 app = webapp.WSGIApplication([('/_ah/xmpp/message/chat/', XMPPHandler), 
-                              ('/cron', GVHandler), 
-                              ('/worker', TaskHandler)], 
+                              (MailHandler.mapping())], 
                               debug=True)
 
 def main():
@@ -127,6 +81,7 @@ if __name__ == '__main__':
     XMPP_USERNAME = GV_USERNAME
     HP = HTMLParser()
     PH = re.compile("\+[0-9]{11}")
+    TO = re.compile("(?<=\.)[0-9]{11}(?=\.)")
 
     voice = GoogleVoice(GV_USERNAME, creds.get_gv_password())
 
